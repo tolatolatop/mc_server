dfa33fb66a83ab2af9ae5899e956ce9d9b87924c
tolatolatop <tolatolatop@gmail.com>
2022-07-20 06:20:07 +0800

    无需使用sudo命令

--- game_server/init.sh
+++ game_server/init.sh
@@ -1,5 +1,4 @@
 curl -O https://gosspublic.alicdn.com/ossutil/1.7.13/ossutil64
-mv ossutil64 ossutil
-chmod 755 ossutil
-sudo mv ossutil /usr/local/bin/ossutil
+chmod 755 ossutil64
+mv ossutil64 /usr/local/bin/ossutil
 ossutil --version
