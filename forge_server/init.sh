49570d0f6482af3ff96b1eb629af546462ab07d0
tolatolatop <tolatolatop@gmail.com>
2022-07-20 06:31:29 +0800

    切换forge版本

--- forge_server/init.sh
+++ forge_server/init.sh
@@ -2,8 +2,8 @@
 set -ex
 mkdir bin && mkdir config && mkdir save_data && mkdir logs
 pushd bin
-mv ../forge-1.18.2-40.1.68-installer.jar .
-java -jar forge-1.18.2-40.1.68-installer.jar --installServer
+mv ../forge-1.18.2-40.1.60-installer.jar .
+java -jar forge-1.18.2-40.1.60-installer.jar --installServer
 bash run.sh
 sed -i 's/eula=false/eula=true/g' eula.txt
 popd
