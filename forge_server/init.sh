b879a6a32c02a00ae7a7bcfce2ea5b8d09d1d282
tolatolatop <tolatolatop@gmail.com>
2022-07-20 04:30:01 +0800

    新增build命令

--- forge_server/init.sh
+++ forge_server/init.sh
@@ -1,11 +1,9 @@
 #!bash
 set -ex
-sed -i 's/eula=false/eula=true/g' eula.txt
 mkdir bin && mkdir config && mkdir save_data && mkdir logs
 pushd bin
-mv forge-1.18.2-40.1.68-installer.jar .
+mv ../forge-1.18.2-40.1.68-installer.jar .
 java -jar forge-1.18.2-40.1.68-installer.jar --installServer
-java @user_jvm_args.txt @libraries/net/minecraftforge/forge/1.18.2-40.1.68/unix_args.txt <<EOF
-stop
-EOF
+bash run.sh
+sed -i 's/eula=false/eula=true/g' eula.txt
 popd
