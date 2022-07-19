81770aaf1d8f31a984b32051d54b7d6e9008bdc4
tolatolatop <tolatolatop@gmail.com>
2022-07-20 06:19:14 +0800

    使用curl命令替换wget

--- game_server/init.sh
+++ game_server/init.sh
@@ -1,4 +1,5 @@
-wget https://gosspublic.alicdn.com/ossutil/1.7.13/ossutil64 -O ossutil
+curl -O https://gosspublic.alicdn.com/ossutil/1.7.13/ossutil64
+mv ossutil64 ossutil
 chmod 755 ossutil
 sudo mv ossutil /usr/local/bin/ossutil
 ossutil --version
